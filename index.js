079343e2519a1d67fea753351c3c536d6f95b201
Wes Todd <wes@wesleytodd.com>
2015-12-30 09:28:05 -0600

    added support for subtypes, serviceDomain, parentDomain and instance

--- index.js
+++ index.js
@@ -6,30 +6,94 @@ var defined = function (name) {
   return name
 }
 
+var flatten = function (arr) {
+  return Array.prototype.concat.apply.bind(Array.prototype.concat, [])(arr)
+}
+
 exports.stringify = function (data) {
-  if (typeof data === 'object' && data && data.name) return exports.stringify(data.name, data.protocol, data.subtypes)
-  return Array.prototype.concat.apply([], arguments).filter(defined).map(prefix).join('.')
+  var domain = [data.serviceDomain, data.parentDomain || 'local'].filter(defined)
+  var service = [data.name, data.protocol]
+
+  // Do we have subtypes?
+  if (data.subtypes && data.subtypes.length) {
+    // _sub has to go to the front of the service
+    service.unshift('sub')
+  }
+
+  return flatten([data.instance, data.subtypes, service.map(prefix), domain].filter(defined)).join('.')
 }
 
 exports.parse = function (str) {
   var parts = str.split('.')
 
+  // Pull off subtypes or instance
+  var subtypes = []
+  if (parts.indexOf('_sub') !== -1) {
+    subtypes = parts.splice(0, parts.indexOf('_sub'))
+
+    // Discard _sub
+    parts.shift()
+  }
+
+  // Remove leading _
   for (var i = 0; i < parts.length; i++) {
     if (parts[i][0] !== '_') continue
     parts[i] = parts[i].slice(1)
   }
 
+  var instance = parts.shift()
+  var name = parts.shift()
+  var protocol = parts.shift()
+
+  // If name is tcp or udp, it means we didnt have an instance name
+  // so protocol goes back, name becomes instance and protocol becomes name
+  // and instance becomes null
+  if (name === 'tcp' || name === 'udp') {
+    parts.unshift(protocol)
+    protocol = name
+    name = instance
+    instance = null
+  }
+
+  var serviceDomain = parts.shift()
+  var parentDomain = parts.shift()
+
+  // Service domain is optional
+  if (!parentDomain) {
+    parentDomain = serviceDomain
+    serviceDomain = null
+  }
+
+  // Trim trailing dot from parent domain if present
+  if (parentDomain && parentDomain[parentDomain.length - 1] === '.') {
+    parentDomain = parentDomain.slice(0, parentDomain.length - 1)
+  }
+
   return {
-    name: parts.shift(),
-    protocol: parts.shift() || null,
-    subtypes: parts
+    instance: instance,
+    name: name,
+    protocol: protocol,
+    serviceDomain: serviceDomain,
+    parentDomain: parentDomain,
+    subtypes: subtypes
   }
 }
 
-exports.tcp = function (name) {
-  return exports.stringify(name, 'tcp', Array.prototype.concat.apply([], Array.prototype.slice.call(arguments, 1)))
+exports.tcp = function (data) {
+  data = data || {}
+  data.protocol = 'tcp'
+  return exports.stringify(data)
 }
 
-exports.udp = function (name) {
-  return exports.stringify(name, 'udp', Array.prototype.concat.apply([], Array.prototype.slice.call(arguments, 1)))
+exports.tcp.http = function (data) {
+  data = data || {}
+  data.protocol = 'tcp'
+  data.name = 'http'
+  return exports.stringify(data)
+}
+
+exports.udp = function (data) {
+  data = data || {}
+  data.protocol = 'udp'
+  return exports.stringify(data)
 }
--- test.js
+++ test.js
@@ -2,25 +2,87 @@ var tape = require('tape')
 var type = require('./')
 
 tape('stringifies', function (t) {
-  t.same(type.stringify('http', 'tcp'), '_http._tcp')
-  t.same(type.stringify('http', 'tcp', 'sub'), '_http._tcp._sub')
-  t.same(type.stringify('http', 'tcp', 'sub', 'sub2'), '_http._tcp._sub._sub2')
-  t.same(type.stringify('http', 'tcp', ['sub', 'sub2']), '_http._tcp._sub._sub2')
-  t.same(type.stringify({name: 'http', protocol: 'tcp', subtypes: ['sub', 'sub2']}), '_http._tcp._sub._sub2')
+  t.same(type.stringify({
+    name: 'http',
+    protocol: 'tcp'
+  }), '_http._tcp.local')
+  t.same(type.stringify({
+    name: 'http',
+    protocol: 'tcp',
+    serviceDomain: 'FoobarsLaptop'
+  }), '_http._tcp.FoobarsLaptop.local')
+  t.same(type.stringify({
+    name: 'http',
+    protocol: 'tcp',
+    serviceDomain: 'FoobarsLaptop',
+    instance: 'TheService'
+  }), 'TheService._http._tcp.FoobarsLaptop.local')
+  t.same(type.stringify({
+    name: 'http',
+    protocol: 'tcp',
+    serviceDomain: 'FoobarsLaptop',
+    subtypes: ['foo']
+  }), 'foo._sub._http._tcp.FoobarsLaptop.local')
+  t.same(type.stringify({
+    name: 'http',
+    protocol: 'tcp',
+    serviceDomain: 'FoobarsLaptop',
+    subtypes: ['foo', 'bar']
+  }), 'foo.bar._sub._http._tcp.FoobarsLaptop.local')
   t.end()
 })
 
 tape('parses', function (t) {
-  t.same(type.parse('_http._tcp'), {name: 'http', protocol: 'tcp', subtypes: []})
-  t.same(type.parse('_http._tcp._sub'), {name: 'http', protocol: 'tcp', subtypes: ['sub']})
-  t.same(type.parse('_http._tcp._sub._sub2'), {name: 'http', protocol: 'tcp', subtypes: ['sub', 'sub2']})
+  t.same(type.parse('_http._tcp.local'), {
+	instance: null,
+    name: 'http',
+    protocol: 'tcp',
+    parentDomain: 'local',
+    serviceDomain: null,
+    subtypes: []
+  })
+  t.same(type.parse('_http._tcp.FoobarsLaptop.local'), {
+	instance: null,
+    name: 'http',
+    protocol: 'tcp',
+    parentDomain: 'local',
+    serviceDomain: 'FoobarsLaptop',
+    subtypes: []
+  })
+  t.same(type.parse('TheService._http._tcp.FoobarsLaptop.local'), {
+	instance: 'TheService',
+    name: 'http',
+    protocol: 'tcp',
+    parentDomain: 'local',
+    serviceDomain: 'FoobarsLaptop',
+    subtypes: []
+  })
+  t.same(type.parse('foo._sub._http._tcp.FoobarsLaptop.local'), {
+	instance: null,
+    name: 'http',
+    protocol: 'tcp',
+    parentDomain: 'local',
+    serviceDomain: 'FoobarsLaptop',
+    subtypes: ['foo']
+  })
+  t.same(type.parse('bar.foo._sub._http._tcp.FoobarsLaptop.local'), {
+	instance: null,
+    name: 'http',
+    protocol: 'tcp',
+    parentDomain: 'local',
+    serviceDomain: 'FoobarsLaptop',
+    subtypes: ['bar', 'foo']
+  })
   t.end()
 })
 
 tape('shorthands', function (t) {
-  t.same(type.tcp('http'), '_http._tcp')
-  t.same(type.tcp('http', 'sub'), '_http._tcp._sub')
-  t.same(type.tcp('http', 'sub', 'sub2'), '_http._tcp._sub._sub2')
-  t.same(type.tcp('http', ['sub', 'sub2']), '_http._tcp._sub._sub2')
+  t.same(type.tcp({
+    name: 'http'
+  }), '_http._tcp.local')
+  t.same(type.udp({
+    name: 'http'
+  }), '_http._udp.local')
+  t.same(type.tcp.http(), '_http._tcp.local')
   t.end()
 })
